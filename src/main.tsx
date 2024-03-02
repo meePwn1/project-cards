399245098b8a33cab982a78174744202afac0b52
detstvo <95248947+meePwn1@users.noreply.github.com>
2024-03-02 07:50:11 +0500

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
 import { App } from '@/App'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
 import ReactDOM from 'react-dom/client'
+import './styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
