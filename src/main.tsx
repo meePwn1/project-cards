e9f9cc037becc13af9163d5076c7fba47927ecb1
detstvo <95248947+meePwn1@users.noreply.github.com>
2024-04-13 21:27:43 +0500

    config env and baseApi

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,6 @@ import ReactDOM from 'react-dom/client'
 
 import './styles/index.scss'
 
-import { App } from './App'
+import { App } from './app/App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
