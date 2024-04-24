24291d8a9cd51ca56fa2f00b18d03c4ac14f1e8e
detstvo <95248947+meePwn1@users.noreply.github.com>
2024-04-24 23:04:18 +0500

    ref: zod schema

--- src/common/schemas/schema.ts
+++ src/common/schemas/schema.ts
@@ -2,8 +2,16 @@ import { z } from 'zod'
 
 export const formSchema = z.object({
   confirmPassword: z.string().min(4),
-  email: z.string().email().nonempty(),
+  deckName: z.string().min(3, 'Deck name is too short').max(30, 'Deck name is too long'),
+  email: z.string().email(),
+  image: z
+    .instanceof(File)
+    .optional()
+    .refine(file => {
+      console.log(file)
+    }),
   password: z.string().min(4),
+  private: z.boolean().optional(),
   rememberMe: z.boolean().optional(),
   username: z.string().min(3),
 })
