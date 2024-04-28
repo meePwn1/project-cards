30c75c0304105c3065e1cc80ba8aa6e8d837eb27
keirWeb <zavalyuk00@list.ru>
2024-04-29 00:38:59 +0200

    fix routes and filters

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,25 @@
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
+
+import { router } from '@/app/router'
+import { ROUTES } from '@/common/constants'
+import { useMeQuery } from '@/services/auth'
 
 import { Header } from '../header'
 
 export const Layout = () => {
+  // const { isError } = useMeQuery()
+
+  // const pathname = window.location.pathname
+
+  // refactoring
+  // if (!isError) {
+  //   if (pathname !== ROUTES.profile) {
+  //     return <Navigate to={ROUTES.home} />
+  //     // router.navigate(ROUTES.home)
+  //   }
+  // }
+
   return (
     <>
       <Header />
