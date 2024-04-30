fd22919682d2bc68482a2d88eda39d4bbe6f7a8b
detstvo <95248947+meePwn1@users.noreply.github.com>
2024-04-30 21:25:12 +0500

    ref: some fix for Modal

--- src/components/ui/modal/Modal.tsx
+++ src/components/ui/modal/Modal.tsx
@@ -1,10 +1,10 @@
 import { ReactNode } from 'react'
 
 import * as Dialog from '@radix-ui/react-dialog'
-import clsx from 'clsx'
 
 import s from './Modal.module.scss'
 
+import { ScrollArea } from '..'
 import { Icon } from '../icon'
 import { Typography } from '../typography'
 
@@ -17,25 +17,25 @@ type Props = {
   trigger?: ReactNode
 }
 export const Modal = (props: Props) => {
-  const { children, modal = false, onOpenChange, open, title, trigger } = props
+  const { children, modal = true, onOpenChange, open, title, trigger } = props
 
   return (
     <Dialog.Root modal={modal} onOpenChange={onOpenChange} open={open}>
       <Dialog.Trigger asChild>{trigger}</Dialog.Trigger>
       <Dialog.Portal>
-        <div className={clsx(s.root, open && s.show)}>
-          <div className={s.wrapper}>
-            <Dialog.Overlay className={s.dialogOverlay} />
-            <Dialog.Content className={s.dialogContent}>
-              <div className={s.header}>
-                <Typography variant={'h3'}>{title}</Typography>
-                <Dialog.Close className={s.closeButton}>
-                  <Icon className={s.closeIcon} name={'common/close'} />
-                </Dialog.Close>
-              </div>
+        <Dialog.Overlay className={s.dialogOverlay} />
+        <div className={s.wrapper}>
+          <Dialog.Content className={s.dialogContent} forceMount>
+            <div className={s.header}>
+              <Typography variant={'h3'}>{title}</Typography>
+              <Dialog.Close className={s.closeButton}>
+                <Icon className={s.closeIcon} name={'common/close'} />
+              </Dialog.Close>
+            </div>
+            <ScrollArea className={s.scrollArea} mh={575}>
               <div className={s.content}>{children}</div>
-            </Dialog.Content>
-          </div>
+            </ScrollArea>
+          </Dialog.Content>
         </div>
       </Dialog.Portal>
     </Dialog.Root>
