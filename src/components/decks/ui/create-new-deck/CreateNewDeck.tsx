d2378e0bbd8d2400c11305e09569648efbec70bf
detstvo <95248947+meePwn1@users.noreply.github.com>
2024-04-30 21:26:41 +0500

    ref: refactor CreateNewDeck

--- src/components/decks/ui/create-new-deck/CreateNewDeck.tsx
+++ src/components/decks/ui/create-new-deck/CreateNewDeck.tsx
@@ -1,19 +1,30 @@
 import { useState } from 'react'
+import { toast } from 'react-toastify'
 
 import { Button, Modal, Typography } from '@/components/ui'
+import { useCreateDeckMutation } from '@/services/decks'
 
 import { CreateDeckForm } from '../forms/create-deck-form'
 
 export const CreateNewDeck = () => {
   const [open, setOpen] = useState(false)
+  const [createDeck, { isLoading }] = useCreateDeckMutation()
+
   const handleSubmit = (data: FormData) => {
-    console.log(data)
+    createDeck(data)
+      .unwrap()
+      .then(() => {
+        setOpen(false)
+        toast.success('Deck created')
+      })
   }
+
+  console.log(isLoading)
 
   return (
     <>
       <Modal onOpenChange={setOpen} open={open} title={'Add New Deck'}>
-        <CreateDeckForm onSubmit={handleSubmit} setOpen={setOpen} />
+        <CreateDeckForm isLoading={isLoading} onSubmit={handleSubmit} setOpen={setOpen} />
       </Modal>
       <Button onClick={() => setOpen(true)}>
         <Typography variant={'subtitle2'}>Add New Deck</Typography>
