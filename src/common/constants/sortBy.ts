bbab335d6b301e33449970ad52272cb3a2a33934
detstvo <95248947+meePwn1@users.noreply.github.com>
2024-05-06 21:51:10 +0500

    ref: add new sorts

--- src/common/constants/sortBy.ts
+++ src/common/constants/sortBy.ts
@@ -1,7 +1,10 @@
 export const SORT_BY = {
+  ANSWER: 'answer',
   AUTHOR_NAME: 'author.name',
   CARDS_COUNT: 'cardsCount',
   CREATED: 'created',
+  GRADE: 'grade',
   NAME: 'name',
+  QUESTION: 'question',
   UPDATED: 'updated',
-}
+} as const
