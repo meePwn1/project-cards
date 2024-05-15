9881c311dc51d4c645aef1177b97e4d991bc0c20
detstvo <95248947+meePwn1@users.noreply.github.com>
2024-05-16 02:36:48 +0500

    ref: refactor EditDeckModal

--- src/components/decks/ui/modals/edit-deck-modal/EditDeckModal.tsx
+++ src/components/decks/ui/modals/edit-deck-modal/EditDeckModal.tsx
@@ -1,18 +1,21 @@
 import { Modal } from '@/components/ui'
+import { DeckById } from '@/services/decks'
 
 import { CreateDeckForm } from '../../forms/create-deck-form'
 
 type Props = {
+  deck: DeckById
   isLoading?: boolean
   onSubmit?: (data: FormData) => void
   open?: boolean
   setOpen?: (value: boolean) => void
 }
 
-export const EditDeckModal = ({ isLoading, onSubmit, open, setOpen }: Props) => {
+export const EditDeckModal = ({ deck, isLoading, onSubmit, open, setOpen }: Props) => {
   return (
     <Modal onOpenChange={setOpen} open={open} title={'Edit deck'}>
       <CreateDeckForm
+        deck={deck}
         isLoading={isLoading}
         onSubmit={onSubmit}
         setOpen={setOpen}
