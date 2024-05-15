0771ce9ddd7e2b8328e77875ec794254029aabd8
detstvo <95248947+meePwn1@users.noreply.github.com>
2024-05-16 02:41:17 +0500

    feat: CardsMoreDropdown

--- src/common/schemas/schema.ts
+++ src/common/schemas/schema.ts
@@ -1,10 +1,12 @@
 import { z } from 'zod'
 
 export const formSchema = z.object({
+  answer: z.string().min(3).max(500),
   confirmPassword: z.string().min(4),
   deckName: z.string().min(3, 'Deck name is too short').max(30, 'Deck name is too long'),
   email: z.string().email(),
   password: z.string().min(4),
+  question: z.string().min(3).max(500),
   rememberMe: z.boolean().optional(),
   username: z.string().min(3),
 })
---
+++ src/components/cards/ui/cards-more-dropdown/CardsMoreDropdown.tsx
@@ -0,0 +1,97 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+
+import { ROUTES } from '@/common/constants'
+import { DeleteDeckModal, EditDeckModal } from '@/components/decks/ui'
+import { DropdownMenu, DropdownMenuItemWithIcon, Icon, Typography } from '@/components/ui'
+import { DeckById, useDeleteDeckMutation, useUpdateDeckMutation } from '@/services'
+
+import s from './CardsMoreDropdown.module.scss'
+
+type Props = {
+  deck?: DeckById
+  isMyDeck?: boolean
+}
+
+export const CardsMoreDropdown = ({ deck, isMyDeck }: Props) => {
+  const [openDeleteDeckModal, setOpenDeleteDeckModal] = useState(false)
+  const [openEditDeckModal, setOpenEditDeckModal] = useState(false)
+  const [deleteDeck, { isLoading: isDeleteDeckLoading }] = useDeleteDeckMutation()
+  const [editDeck, { isLoading: isEditDeckLoading }] = useUpdateDeckMutation()
+  const navigate = useNavigate()
+
+  // handlers
+  const handleLearnCards = () => {
+    navigate(ROUTES.LEARN(deck?.id || ''))
+  }
+
+  const handleOpenDeleteDeckModal = () => {
+    setOpenDeleteDeckModal(true)
+  }
+  const handleDeleteDeck = () => {
+    deleteDeck({ id: deck?.id || '' })
+      .unwrap()
+      .then(() => {
+        setOpenDeleteDeckModal(false)
+        toast.success(`${deck?.name} deleted successfully`)
+        navigate(-1)
+      })
+  }
+  const handleOpenEditDeckModal = () => {
+    setOpenEditDeckModal(true)
+  }
+  const handleEditDeck = (data: FormData) => {
+    editDeck({ data, id: deck?.id || '' })
+      .unwrap()
+      .then(() => {
+        setOpenEditDeckModal(false)
+        navigate(-1)
+        toast.success(`${deck?.name} updated successfully`)
+      })
+  }
+
+  return (
+    <>
+      <DeleteDeckModal
+        deckName={deck?.name}
+        isLoading={isDeleteDeckLoading}
+        onConfirm={handleDeleteDeck}
+        open={openDeleteDeckModal}
+        setOpen={setOpenDeleteDeckModal}
+      />
+      {deck && (
+        <EditDeckModal
+          deck={deck}
+          isLoading={isEditDeckLoading}
+          onSubmit={handleEditDeck}
+          open={openEditDeckModal}
+          setOpen={setOpenEditDeckModal}
+        />
+      )}
+
+      {isMyDeck && (
+        <DropdownMenu className={s.dropdown} trigger={<Icon name={'common/more'} />}>
+          <DropdownMenuItemWithIcon
+            icon={<Icon name={'common/play'} size={16} />}
+            onSelect={handleLearnCards}
+          >
+            <Typography variant={'caption'}>Learn</Typography>
+          </DropdownMenuItemWithIcon>
+          <DropdownMenuItemWithIcon
+            icon={<Icon name={'common/edit'} size={16} />}
+            onSelect={handleOpenEditDeckModal}
+          >
+            <Typography variant={'caption'}>Edit</Typography>
+          </DropdownMenuItemWithIcon>
+          <DropdownMenuItemWithIcon
+            icon={<Icon name={'common/trash'} size={16} />}
+            onSelect={handleOpenDeleteDeckModal}
+          >
+            <Typography variant={'caption'}>Delete</Typography>
+          </DropdownMenuItemWithIcon>
+        </DropdownMenu>
+      )}
+    </>
+  )
+}
