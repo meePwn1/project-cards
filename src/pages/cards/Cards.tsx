d67a72de79d3ff1f30f462dc46a2b9c140ac9bfa
detstvo <95248947+meePwn1@users.noreply.github.com>
2024-05-24 21:40:03 +0500

    ref: refactor Cards page

--- src/pages/cards/Cards.tsx
+++ src/pages/cards/Cards.tsx
@@ -1,31 +1,37 @@
-import { Link, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
-import { PARAMS, ROUTES } from '@/common/constants'
-import { SortType } from '@/common/types'
-import { getSortString } from '@/common/utils'
 import { BackToPage } from '@/components/back-to-page'
-import { CardsMoreDropdown, CardsTable, CreateNewCard, EmptyCards } from '@/components/cards/ui'
+import {
+  CardsContent,
+  CardsHeaderButton,
+  CardsMoreDropdown,
+  EmptyCards,
+} from '@/components/cards/ui'
 import { DeckCover } from '@/components/cards/ui/deck-cover'
 import { Page } from '@/components/layout'
-import { Button, LinearLoader, Pagination, TextField, Typography } from '@/components/ui'
+import { LinearLoader, Typography } from '@/components/ui'
 import { useGetCardsQuery, useGetDeckByIdQuery, useMeQuery } from '@/services'
 
 import s from './Cards.module.scss'
 
-import { PER_PAGE, useCardsQueryParams } from './lib'
-
-const perPageOptions = [5, 10, 15]
+import { useCardsQueryParams } from './lib'
 
 export const Cards = () => {
   const { deckId } = useParams()
 
-  const { page, perPage, queryParams, question, searchParams, setSearchParams, sort } =
-    useCardsQueryParams({ deckId })
+  const { page, perPage, queryParams, question, searchError, sort } = useCardsQueryParams()
 
   // queries
   const { data: me, isLoading: isMeLoading } = useMeQuery()
   const { data: deck, isLoading: isDeckLoading } = useGetDeckByIdQuery({ id: deckId || '' })
-  const { data: cards, isLoading: isCardsLoading } = useGetCardsQuery(queryParams)
+  const {
+    data: cards,
+    isFetching: isCardsFetching,
+    isLoading: isCardsLoading,
+  } = useGetCardsQuery({
+    id: deckId || '',
+    params: queryParams,
+  })
 
   const isMyDeck = me?.id === deck?.userId
   const isEmptyDeck = cards?.items?.length === 0
@@ -34,91 +40,30 @@ export const Cards = () => {
     return <LinearLoader />
   }
 
-  const handlePageChange = (currentPage: number) => {
-    if (currentPage > 1) {
-      searchParams.set(PARAMS.PAGE, currentPage.toString())
-    } else {
-      searchParams.delete(PARAMS.PAGE)
-    }
-    setSearchParams(searchParams)
-  }
-
-  const handlePerPageChange = (perPage: number) => {
-    if (perPage !== PER_PAGE) {
-      searchParams.set(PARAMS.PER_PAGE, perPage.toString())
-    } else {
-      searchParams.delete(PARAMS.PER_PAGE)
-    }
-    setSearchParams(searchParams)
-  }
-
-  const handleQuestionChange = (question: string) => {
-    if (question !== '') {
-      searchParams.set(PARAMS.SEARCH, question)
-    } else {
-      searchParams.delete(PARAMS.SEARCH)
-    }
-    setSearchParams(searchParams)
-  }
-
-  const handleSortChange = (sort: SortType) => {
-    const sortString = getSortString(sort)
-
-    if (sortString) {
-      searchParams.set(PARAMS.SORTING, sortString)
-    } else {
-      searchParams.delete(PARAMS.SORTING)
-    }
-    setSearchParams(searchParams)
-  }
-
   return (
     <Page pt={24}>
       <BackToPage className={s.back} />
-      {window.innerWidth < 768 && <div>asds</div>}
       <div className={s.pageHeader}>
         <div className={s.deckInfo}>
           <Typography variant={'h1'}>{deck?.name}</Typography>
           {isMyDeck && <CardsMoreDropdown deck={deck} />}
         </div>
-        {!isEmptyDeck &&
-          (isMyDeck ? (
-            <CreateNewCard />
-          ) : (
-            <Button as={Link} to={ROUTES.LEARN(deckId)}>
-              Learn to Pack
-            </Button>
-          ))}
+        {!isEmptyDeck && <CardsHeaderButton deckId={deckId} isMyDeck={isMyDeck} />}
       </div>
       <DeckCover cover={deck?.cover} />
-      <TextField
-        className={s.search}
-        onValueChange={handleQuestionChange}
-        placeholder={'Search by question'}
-        search
-        value={question}
-      />
+
       {isEmptyDeck ? (
         <EmptyCards isMyDeck={isMyDeck} />
       ) : (
-        <>
-          <CardsTable
-            cards={cards?.items}
-            isMyDeck={isMyDeck}
-            onSort={handleSortChange}
-            sort={sort}
-          />
-        </>
-      )}
-      {cards?.pagination.totalItems && cards.pagination.totalPages > 1 && (
-        <Pagination
-          className={s.pagination}
-          count={cards.pagination.totalPages}
-          onPageChange={handlePageChange}
-          onPerPageChange={handlePerPageChange}
+        <CardsContent
+          cards={cards}
+          isCardsFetching={isCardsFetching}
+          isMyDeck={isMyDeck}
           page={page}
           perPage={perPage}
-          perPageOptions={perPageOptions}
+          question={question}
+          searchError={searchError}
+          sort={sort}
         />
       )}
     </Page>
